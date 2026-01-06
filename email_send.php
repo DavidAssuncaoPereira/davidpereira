5d2b0f0513714ecf4144357ba580161fa534c262
DavidAssuncaoPereira <3hs.david.pereira@gmail.com>
2026-01-06 12:55:54 +0000

    Form

--- email_send.php
+++ email_send.php
@@ -1,8 +1,25 @@
 <?php
-$to = "3hs.david.pereira@gmail.com";
-$subject = $_POST['subject'];
-$message = "Nome: " . $_POST['nome'] . "\nEmail: " . $_POST['message'];
-$headers = "From: ". $_POST['email'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-mail($to, $subject, $message, $headers);
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "3hs.david.pereira@gmail.com";
+
+    $body = "Nome: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Mensagem:\n$message";
+
+    $headers  = "From: Site David Pereira <site@seudominio.com>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem.";
+    }
+}
 ?>
